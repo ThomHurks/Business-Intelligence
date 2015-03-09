b014cae86e425336c9e49ddfbb81806546cd8bca
Thom Hurks <thomhurks@gmail.com>
2015-03-09 15:50:17 +0100

    Added missing value and range analysis.
    Added missing value and range analysis.

--- 1BM56-term/term_assignment.m
+++ 1BM56-term/term_assignment.m
@@ -10,3 +10,24 @@ unique_columns = cell(17,1);
 for i=1:17
 unique_columns(i) =  {unique(data(:,i))};
 end;
+% Get the nr of missing values in each column, so they can be evaluated.
+missing_columns = cell(17,3);
+for i=1:17
+    curArray = table2array(data(:,i));
+    if isnumeric(curArray)
+        missing_columns(i,1) =  {sum(isnan(curArray))};
+    end;
+    missing_columns(i,2) =  {sum(strncmp('unknown',curArray, 7))};
+    if iscategorical(curArray)
+        missing_columns(i,3) =  {sum(isundefined(curArray))};
+    end;
+end;
+% Get the value ranges in each column, so they can be evaluated.
+range_columns = cell(17,2);
+for i=1:17
+    curArray = table2array(data(:,i));
+    if isnumeric(curArray)
+        range_columns(i,1) =  {min(curArray)};
+        range_columns(i,2) =  {max(curArray)};
+    end;
+end;
