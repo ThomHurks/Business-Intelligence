0fc3c4bb64409c0e90dec3e3a880e2d16218e13f
Thom Hurks <thomhurks@gmail.com>
2015-03-16 14:45:36 +0100

    Added total missing values column, get categorical arrays where possible, fixed unknown counting.
    Added total missing values column.
    Get categorical arrays where possible.
    Fixed unknown value counting.

--- 1BM56-term/term_assignment.m
+++ 1BM56-term/term_assignment.m
@@ -6,21 +6,34 @@ rng(0,'twister');
 % data is the input table, provided by the teachers, it's inside 1BM56-term.mat.
 load('1BM56-term.mat');
 % Get the unique values in each column, so they can be evaluated.
-unique_columns = cell(17,1);
+% Store categorical values separately for easy evaluation.
+unique_columns = cell(17,2);
 for i=1:17
-unique_columns(i) =  {unique(data(:,i))};
+    curTable = unique(data(:,i));
+    unique_columns(i,1) = {curTable};
+    curArray = table2array(curTable);
+    if iscategorical(curArray)
+        unique_columns(i,2) = {categorical(curArray)};
+    end;
 end;
 % Get the nr of missing values in each column, so they can be evaluated.
-missing_columns = cell(17,3);
+missing_columns = cell(17,4);
 for i=1:17
-    curArray = table2array(data(:,i));
+    curData = data(:,i);
+    curArray = table2array(curData);
+    % Count NaN's
     if isnumeric(curArray)
         missing_columns(i,1) =  {sum(isnan(curArray))};
     end;
-    missing_columns(i,2) =  {sum(strncmp('unknown',curArray, 7))};
     if iscategorical(curArray)
+        % Count "unknown" entries.
+        missing_columns(i,2) = {length(find(curArray=='"unknown"'))};
+        % Count <undefined> entries
         missing_columns(i,3) =  {sum(isundefined(curArray))};
     end;
+    % Count the total of missing values
+    missing_values = { NaN, '', '<undefined>', '"unknown"' };
+    missing_columns(i,4) = {sum(ismissing(data(:,i), missing_values))};
 end;
 % Get the value ranges in each column, so they can be evaluated.
 range_columns = cell(17,2);
