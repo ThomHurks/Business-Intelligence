c0e63895884553d5a92e010957de778b9c497887
Vincent de Vos <vin.de.vos@gmail.com>
2015-03-18 12:43:18 +0100

    Balanced data set

--- 1BM56-term/term_assignment.m
+++ 1BM56-term/term_assignment.m
@@ -3,8 +3,10 @@
 
 % For reproducibility:
 rng(0,'twister');
+
 % data is the input table, provided by the teachers, it's inside 1BM56-term.mat.
 load('1BM56-term.mat');
+
 % Get the unique values in each column, so they can be evaluated.
 % Store categorical values separately for easy evaluation.
 unique_columns = cell(17,2);
@@ -16,6 +18,7 @@ for i=1:17
         unique_columns(i,2) = {categorical(curArray)};
     end;
 end;
+
 % Get the nr of missing values in each column, so they can be evaluated.
 missing_columns = cell(17,4);
 for i=1:17
@@ -35,6 +38,7 @@ for i=1:17
     missing_values = { NaN, '', '<undefined>', '"unknown"' };
     missing_columns(i,4) = {sum(ismissing(data(:,i), missing_values))};
 end;
+
 % Get the value ranges in each column, so they can be evaluated.
 range_columns = cell(17,2);
 for i=1:17
@@ -44,3 +48,25 @@ for i=1:17
         range_columns(i,2) =  {max(curArray)};
     end;
 end;
+
+% Question 2 - Balance data sets 
+% Subscribed 30% vs Unsubscribed 70%
+
+% Divide original data set in set of subscribed and unsubscribed data
+set_subscribed = data(find(table2array(data(:,17)) == '"yes"'),:);
+set_unsubscribed = data(find(table2array(data(:,17)) == '"no"'),:);
+
+balanced_size_subscribed = size(set_subscribed,1);
+balanced_size_unsubscribed = (70*balanced_size_subscribed)/30;
+balanced_unsubscribed = datasample(set_unsubscribed, balanced_size_unsubscribed);
+
+% Create a the balanced dataset where 30% is subscribed and 70% is unsubscribed data
+bdata = vertcat(set_subscribed, balanced_unsubscribed);
+% Shuffle our rows 
+bdata = bdata(randperm(size(bdata,1)),:);
+
+% Data set for Neural Networks 
+
+% Data set for Fuzzy Inference Model 
+
+
