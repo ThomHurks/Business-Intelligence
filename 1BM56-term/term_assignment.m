a71cdb336a17df9837f5aa524d30216928a6cae5
Thom Hurks <thomhurks@gmail.com>
2015-04-06 23:57:05 +0200

    Improved balancing for final test set, NN works.
    - Working Neural Network.
    - Improved balancing for final test set ; now keeps original data set
    imbalance.

--- 1BM56-term/term_assignment.m
+++ 1BM56-term/term_assignment.m
@@ -7,31 +7,13 @@ rng(0,'twister');
 % data is the input table, provided by the teachers, it's inside 1BM56-term.mat.
 load('1BM56-term.mat');
 
-% Divide original data set in set of subscribed and unsubscribed data
-set_subscribed = data(table2array(data(:,17)) == '"yes"',:);
-set_unsubscribed = data(table2array(data(:,17)) == '"no"',:);
-
-% Get nr of rows (dimension is 1) in matrix.
-balanced_size_subscribed = size(set_subscribed,1);
-% Calculate number of unsubscribed samples to get desired 70/30 ratio.
-balanced_size_unsubscribed = (70*balanced_size_subscribed)/30;
-% Sample k observations uniformly at random, without replacement.
-balanced_unsubscribed = datasample(set_unsubscribed, balanced_size_unsubscribed,...
-                                   'Replace', false);
-% Concatenate the subscribed and sampled unsubcribed matrices to create
-% a 70/30 balanced dataset.
-balanced_data = vertcat(set_subscribed, balanced_unsubscribed);
-% Shuffle the rows of the matrix by doing a random permutation from
-% 1 to the row count of the matrix.
-balanced_data = balanced_data(randperm(size(balanced_data,1)),:);
-
 % Create the empty matrix that will contain the normalized data.
-normalized_data = zeros(size(balanced_data, 1),0);
+normalized_data = zeros(size(data, 1),0);
 
 % Normalization:
 % Loop over all columns, column 17 (outcome) we do separately.
 for i=1:16
-    curArray = table2array(balanced_data(:,i));
+    curArray = table2array(data(:,i));
     % Normalize categorical data by turning them into logical vectors.
     if iscategorical(curArray)
         categories = unique(curArray);
@@ -61,43 +43,68 @@ for i=1:16
     end;
 end;
 % Now do the last column (outcome), convert yes/no to 1 and 0 (binary).
-curArray = table2array(balanced_data(:,17));
+curArray = table2array(data(:,17));
 normalized_data = horzcat(normalized_data, curArray == '"yes"');
 
-% TODO: finish code below this comment.
+% Create final test set, training set and validation set:
+
+% Sample 20% observations uniformly at random, without replacement, for the
+% final test set.
+final_test_set_size = floor(size(normalized_data, 1) * 0.1);
+[final_test_set, final_test_idx] = datasample(normalized_data, final_test_set_size ,...
+                                   'Replace', false);
+train_val_data = removerows(normalized_data, final_test_idx);
+% Divide remaining training/validation set in set of subscribed and 
+% unsubscribed data.
+last_column = size(train_val_data, 2);
+set_subscribed = train_val_data(train_val_data(:,last_column) == 1,:);
+set_unsubscribed = train_val_data(train_val_data(:,last_column) == 0,:);
+% Get nr of subscribed entries.
+balanced_size_subscribed = size(set_subscribed, 1);
+% Calculate number of unsubscribed samples to get desired 70/30 ratio.
+balanced_size_unsubscribed = floor((70 * balanced_size_subscribed) / 30);
+% Sample k observations uniformly at random, without replacement.
+balanced_unsubscribed = datasample(set_unsubscribed, balanced_size_unsubscribed,...
+                                   'Replace', false);
+% Concatenate the subscribed and sampled unsubcribed matrices to create
+% a 70/30 balanced dataset.
+balanced_data = vertcat(set_subscribed, balanced_unsubscribed);
+% Shuffle the rows of the matrix by doing a random permutation from
+% 1 to the row count of the matrix.
+balanced_data = balanced_data(randperm(size(balanced_data, 1)),:);
+train_val_size = size(balanced_data, 1);
+% Add the final test set back to the end of the dataset.
+balanced_data = vertcat(balanced_data, final_test_set);
+
+nn_Inputs = balanced_data(:,(1:(last_column - 1)));
+nn_Labels = balanced_data(:,last_column);
+
+% Create feedforward neural network with 2 hidden layers.
+% First hidden layer has 10 neurons, and second 2 neurons.
+% Training function is the default.
+% net = patternnet([100 20], 'trainlm');
+net = fitnet([10, 2], 'trainlm');
 
 % Specify train, validation and test sets on the neural network.
 % The data is divided by index, the indices for the three subsets are
 % defined by the division parameters trainInd, valInd and testInd.
+trainSize = train_val_size - final_test_set_size;
 net.divideFcn = 'divideind';
-net.divideParam.trainInd = 1:400; % 79% of data
-net.divideParam.valInd = 401:450; % 10% of data
-net.divideParam.testInd = 451:506; % 11% of data
+net.divideParam.trainInd = 1:trainSize;
+net.divideParam.valInd = (trainSize + 1):train_val_size;
+net.divideParam.testInd = (train_val_size + 1):size(balanced_data, 1);
 
+% Train the neural network with the data and get the output for the test
+% set.
+[net, train_record] = train(net, transpose(nn_Inputs), transpose(nn_Labels));
 
-nnInputs = normalized_data(:,1:16);
-nnLabels = normalized_data(:,17); % we need to convert yes/no to 1/0
-
-% Create ranges for different set sizes: train, validate, test
-nncount = size(nnInputs,1);
-trainRange = 1:(nncount*0.6); % 60%
-validateRange = (floor(nncount*0.6)+1):floor(nncount*0.8); % 20%
-testRange = (floor(nncount*0.8)+1):floor(nncount*1.0); % 20%
-
-% Setup our Neural Network
-nnet = patternnet([100 20], 'trainlm');
-nnet.divideFcn = 'divideind';
-nnet.divideParam.trainInd = trainRange;
-nnet.divideParam.valInd = validateRange;
-nnet.divideParam.testInd = testRange;
-
-% train the neural network and get the output for the test set
-[net, tr] = train(nnet, nnInputs, nnLabels);
-output = nnet(nnInputs(:,testRange));
+% Show the trained neural network.
+% view(random_net);
+% view(indices_net);
+% Test the performance of the neural networks using the final test set.
+results = net(nn_Inputs(train_record.testInd));
+nn_perf = perform(net, nn_Labels(train_record.testInd), results);
 
 % Data set for Fuzzy Inference Model 
 
 % TODO %
-
-
-
