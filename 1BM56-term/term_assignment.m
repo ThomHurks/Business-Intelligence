1c610ba90d35f1d5dfed6ce392c6ff705ded857f
Thom Hurks <thomhurks@gmail.com>
2015-04-06 21:25:18 +0200

    Improved normalization
    Improved normalization:
    - Now properly normalizes categorical data.
    - Now normalizes numerical data.
    - Show error when data is not categorical or numerical when normalizing.

--- 1BM56-term/term_assignment.m
+++ 1BM56-term/term_assignment.m
@@ -66,36 +66,44 @@ balanced_unsubscribed = datasample(set_unsubscribed, balanced_size_unsubscribed,
                                    'Replace', false);
 % Concatenate the subscribed and sampled unsubcribed matrices to create
 % a 70/30 balanced dataset.
-bdata = vertcat(set_subscribed, balanced_unsubscribed);
+balanced_data = vertcat(set_subscribed, balanced_unsubscribed);
 % Shuffle the rows of the matrix by doing a random permutation from
 % 1 to the row count of the matrix.
-bdata = bdata(randperm(size(bdata,1)),:);
+balanced_data = balanced_data(randperm(size(balanced_data,1)),:);
 
-% Normalize the data into vectors
-ndata = cell(size(bdata));
+% Create the empty matrix that will contain the normalized data.
+normalized_data = zeros(size(balanced_data, 1),0);
 
-% loop over every column
-for i=1:17 
-    % get all the row data for the column:c
-    cdata = bdata(:,i);
-    
-    % TODO: alleen hebben we voor elke NaN value een entry in onze vector
-    % deze kunnen we met het cleanen eruit halen dan zal het goed gaan
-    udata = table2array(unique(cdata));
-    usize = size(udata,1);
-    
-    % loop over every row (size 1) in the column
-    for c=1:size(cdata,1)
-        % create empty zeros vector
-        vector = zeros(usize,1);
-        % find index for value in the zero vector
-        vindex = find(udata == cdata{c,1});
-        % set this index 1
-        vector(vindex) = 1;
-        % store this vector in our new normalized data table
-        % row:c and column:i 
-        % vec2str = strtrim(cellstr(num2str(vector)));
-        ndata(c,i) = { vector };
+% Normalization:
+% Loop over all columns, column 17 (outcome) we do separately.
+for i=1:16
+    curArray = table2array(balanced_data(:,i));
+    % Normalize categorical data by turning them into logical vectors.
+    if iscategorical(curArray)
+        categories = unique(curArray);
+        % Remove <undefined>s from categories.
+        % <undefined> then becomes a zero-vector.
+        categories = categories(~isundefined(categories));
+        % Category matrix: each row will become a vector of zeroes and
+        % ones, where the 1 indicates the category of that row.
+        cat_matrix = zeros(size(curArray, 1), size(categories, 1));
+        % For each entry in the column, create the logical vector and
+        % put it in the matrix.
+        for j=1:size(curArray, 1)
+            cat_matrix(j,:) = categories == curArray(j);
+        end;
+        % Concatenate to the matrix containing the normalized data.
+        normalized_data = horzcat(normalized_data, cat_matrix);
+    % Normalize numerical data by numerical normalization; range to [0-1]
+    elseif isnumeric(curArray)
+        largest = max(curArray);
+        smallest = min(curArray);
+        range = largest - smallest;
+        curArray = (curArray - smallest) / range;
+        % Concatenate to the matrix containing the normalized data.
+        normalized_data = horzcat(normalized_data, curArray);
+    else
+        display(strcat('Error normalizing column ', num2str(i)))
     end;
 end;
 
@@ -105,8 +113,8 @@ end;
 
 % TODO: alles staat nu dus in een CellTable 
 % Hoe gaan we deze genormalizeerde vector data in onze NN gebruiken?
-nnInputs = ndata(:,1:16);
-nnLabels = ndata(:,17); % we need to convert yes/no to 1/0
+nnInputs = normalized_data(:,1:16);
+nnLabels = normalized_data(:,17); % we need to convert yes/no to 1/0
 
 % Create ranges for different set sizes: train, validate, test
 nncount = size(nnInputs,1);
