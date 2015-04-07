5ab9b6b3f6fdba2ed48832034dc8175cd29ca820
Vincent de Vos <vin.de.vos@gmail.com>
2015-04-07 04:03:01 +0200

    FIS

--- 1BM56-term/term_assignment.m
+++ 1BM56-term/term_assignment.m
@@ -4,7 +4,7 @@
 % For reproducibility:
 rng(0,'twister');
 
-% Data is the input table, provided by the teachers, it's inside 1BM56-term.mat.
+% data is the input table, provided by the teachers, it's inside 1BM56-term.mat.
 load('1BM56-term.mat');
 
 % Create the empty matrix that will contain the normalized data.
@@ -45,17 +45,14 @@ end;
 % Now do the last column (outcome), convert yes/no to 1 and 0 (binary).
 curArray = table2array(data(:,17));
 normalized_data = horzcat(normalized_data, curArray == '"yes"');
-display('Finished normalizing data');
 
 % Create final test set, training set and validation set:
 
-% Sample 10% observations uniformly at random, without replacement, for the
-% final test set. We want the final test set to have the same imbalance
-% as the original test set, to make the test more representative.
+% Sample 20% observations uniformly at random, without replacement, for the
+% final test set.
 final_test_set_size = floor(size(normalized_data, 1) * 0.1);
 [final_test_set, final_test_idx] = datasample(normalized_data, final_test_set_size ,...
                                    'Replace', false);
-% Separate the final test data from the validation/train data for now.
 train_val_data = removerows(normalized_data, final_test_idx);
 % Divide remaining training/validation set in set of subscribed and 
 % unsubscribed data.
@@ -76,132 +73,79 @@ balanced_data = vertcat(set_subscribed, balanced_unsubscribed);
 % 1 to the row count of the matrix.
 balanced_data = balanced_data(randperm(size(balanced_data, 1)),:);
 train_val_size = size(balanced_data, 1);
-% Add the final test set back to the end of the dataset, since the neural
-% network expects one contiguous dataset.
+% Add the final test set back to the end of the dataset.
 balanced_data = vertcat(balanced_data, final_test_set);
-display('Finished balancing data');
 
-% Split the balanced dataset into inputs and labels for the neural network.
-% We need to transpose the input/labels since the neural network
-% expects that each input entry is a column vector and the labels is a row.
-nn_Inputs = transpose(balanced_data(:,(1:(last_column - 1))));
-nn_Labels = transpose(balanced_data(:,last_column));
+nn_Inputs = balanced_data(:,(1:(last_column - 1)));
+nn_Labels = balanced_data(:,last_column);
 
 % Create feedforward neural network with 2 hidden layers.
 % First hidden layer has 10 neurons, and second 2 neurons.
 % Training function is the default.
-% alternative: net = patternnet([100 20], 'trainlm');
+% net = patternnet([100 20], 'trainlm');
 net = fitnet([10, 2], 'trainlm');
 
 % Specify train, validation and test sets on the neural network.
 % The data is divided by index, the indices for the three subsets are
 % defined by the division parameters trainInd, valInd and testInd.
-% Train size is 80% of the balanced train/validation set.
-trainSize = floor(train_val_size * 0.8);
+trainSize = train_val_size - final_test_set_size;
 net.divideFcn = 'divideind';
 net.divideParam.trainInd = 1:trainSize;
 net.divideParam.valInd = (trainSize + 1):train_val_size;
-% The final test set is the unbalanced last part of the balanced_data
-% array that we concatenated back in line 81.
 net.divideParam.testInd = (train_val_size + 1):size(balanced_data, 1);
 
 % Train the neural network with the data and get the output for the test
 % set.
-[net, train_record] = train(net, nn_Inputs, nn_Labels);
-display('Finished training neural network');
+[net, train_record] = train(net, transpose(nn_Inputs), transpose(nn_Labels));
 
 % Show the trained neural network.
-view(net);
+% view(random_net);
+% view(indices_net);
 % Test the performance of the neural networks using the final test set.
 results = net(nn_Inputs(train_record.testInd));
 nn_perf = perform(net, nn_Labels(train_record.testInd), results);
-% Save the neural network to disk, in a file called "<name>_<timestamp>.m"
-timestamp = strcat(datestr(clock,'yyyy-mm-dd-HHMM'),'m',datestr(clock,'ss'),'s');
-save(strcat('T27_neural_net_', timestamp), 'net', 'train_record');
 
 % Data set for Fuzzy Inference Model
 
-% Variables from our original data set
-% age - column 1
-% education - column - 4
-% balance - column 6
-% mortgage - column 7
-% loan - column 8
-% campaign - column 13
-% pdays - number of days since last contact - column 14
-%
+% age(1), balance(6), pdays:number of contacts(14)
 
-% Select the choosen variables for our FIS
-% TODO: choose the correct column index from normalized data 
-% fis_data_input = normalized_data(:,[1 4 6 7 8 13 14]);
-fis_data_input = normalized_data(:,[1 4]);
-
-% Make sure to add the actual labels as the last column 
+fis_data_input = normalized_data(:,[1 2]);
+% fis_data_input = table2array(data(:,[1 6 14]));
 fis_data_labels = table2array(data(:,17)) == '"yes"';
+fis_data_train = fis_data_input(:,:);
 fis_data_train = horzcat(fis_data_input, fis_data_labels);
+%fis_data_train = fis_data_train(1:1000,:);
 
-% For now select a subset for testing (large sets can take quite some time)
-fis_data_train = fis_data_train(1:100,:);
-
-% Generate FIS structure with linear membership functions
 fis_linear = genfis1(fis_data_train, 2, 'gbellmf', 'linear');
 fis = anfis(fis_data_train, fis_linear, size(fis_data_train));
-gensurf(fis);
+%gensurf(fis);
 
-% For every input variable define a set of fuzzy rules 
+fis_linear = addvar(fis_linear,'input','x',[18 95]); % the actual column range 
+fis_linear = addmf(fis_linear,'input',1,'young adult','gaussmf',[18 28]);
+fis_linear = addmf(fis_linear,'input',1,'adult','gaussmf',[29 40]);
+fis_linear = addmf(fis_linear,'input',1,'older adult','gaussmf',[41 60]);
+fis_linear = addmf(fis_linear,'input',1,'elderly','gaussmf',[61 95]);
 
-index = 1;
-
-% age - column 1 (#1)
-fis_linear = addvar(fis_linear,'input','age',[18 95]); % the actual column range
-fis_linear = addmf(fis_linear,'input',index,'young adult','gaussmf',[18 28]);
-fis_linear = addmf(fis_linear,'input',index,'adult','gaussmf',[29 40]);
-fis_linear = addmf(fis_linear,'input',index,'older adult','gaussmf',[41 60]);
-fis_linear = addmf(fis_linear,'input',index,'old','gaussmf',[61 95]);
-
-index = index + 1;
-
-% education - column 4 (#2)
-%fis_linear = addvar(fis_linear,'input','education',[0 20]); 
-%fis_linear = addmf(fis_linear,'input',2,'low educated','gaussmf',[0 8]);
-%fis_linear = addmf(fis_linear,'input',2,'med educated','gaussmf',[8 14]);
-%fis_linear = addmf(fis_linear,'input',2,'highly educated','gaussmf',[14 18]);
-%fis_linear = addmf(fis_linear,'input',2,'academic','gaussmf',[18 20]);
-%index = index + 1;
-
-% balance - column 6 (#3)
 fis_linear = addvar(fis_linear,'input','y',[-8019 102127]);
-fis_linear = addmf(fis_linear,'input',index,'very poor','gaussmf',[-8019 -5000]);
-fis_linear = addmf(fis_linear,'input',index,'poor','gaussmf',[-5000 0]);
-fis_linear = addmf(fis_linear,'input',index,'moderate','gaussmf',[1 5000]);
-fis_linear = addmf(fis_linear,'input',index,'rich','gaussmf',[5001 10000]);
-fis_linear = addmf(fis_linear,'input',index,'very rich','gaussmf',[10001 102127]);
-index = index + 1;
+fis_linear = addmf(fis_linear,'input',2,'very poor','gaussmf',[-8019 -5000]);
+fis_linear = addmf(fis_linear,'input',2,'poor','gaussmf',[-5000 0]);
+fis_linear = addmf(fis_linear,'input',2,'moderate','gaussmf',[1 5000]);
+fis_linear = addmf(fis_linear,'input',2,'rich','gaussmf',[5001 10000]);
+fis_linear = addmf(fis_linear,'input',2,'very rich','gaussmf',[10001 102127]);
 
-% TODO
-% mortgage - column 7
-% loan - column 8
-% campaign - column 13
-
-% pdays - number of days since last contact - column 14
 % input error als ik deze column (column 14 uit data variable) wil meenemen
 %fis_linear = addvar(fis_linear,'input','contact',[0 871]);
-%fis_linear = addmf(fis_linear,'input',index, 'days','gaussmf',[0 7]);
-%fis_linear = addmf(fis_linear,'input',index, 'weeks','gaussmf',[8 30]);
-%fis_linear = addmf(fis_linear,'input',index, 'months','gaussmf',[31 90]);
-%fis_linear = addmf(fis_linear,'input',index, 'many months','gaussmf',[90 365]);
-%fis_linear = addmf(fis_linear,'input',index, 'years','gaussmf',[365 871]);
+%fis_linear = addmf(fis_linear,'input',3, 'days','gaussmf',[0 7]);
+%fis_linear = addmf(fis_linear,'input',3, 'weeks','gaussmf',[8 30]);
+%fis_linear = addmf(fis_linear,'input',3, 'months','gaussmf',[31 90]);
+%fis_linear = addmf(fis_linear,'input',3, 'many months','gaussmf',[90 365]);
+%fis_linear = addmf(fis_linear,'input',3, 'years','gaussmf',[365 871]);
 
-fis_linear = addvar(fis_linear,'output','label', [0 1]);
+fis_linear = addvar(fis_linear,'output','z', [0 1]);
 fis_linear = addmf(fis_linear,'output',1,'no','gaussmf',[0 0]);
 fis_linear = addmf(fis_linear,'output',1,'yes','gaussmf',[1 1]);
 
-%plotmf(fis_linear, 'input', 1)
-%plotmf(fis_linear, 'input', 2)
-%plotmf(fis_linear, 'input', 3)
-%plotmf(fis_linear, 'ouput', 1)
-
-timestamp = strcat(datestr(clock,'yyyy-mm-dd-HHMM'),'m',datestr(clock,'ss'),'s');
-save(strcat('T27_fuzzy_inference_', timestamp), 'fis', 'train_record');
-
-display('Done!');
+plotmf(fis_linear, 'input', 1)
+% plotmf(fis_linear, 'input', 2)
+% Error..
+% plotmf(fis_linear, 'ouput', 1)
